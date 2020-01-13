7c3d09b975e1eb3ee2e4a36c6326a9a010759bab
farukm33 <57073536+farukm33@users.noreply.github.com>
2020-01-13 16:20:40 +0100

    sdas

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+   
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+	
+
+    $('.portfolio-img').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+	
+	var windowWidth = $(window).width();
+    if (windowWidth > 757) {
+
+        
+          
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 500) {
+                    $('.navbar').fadeIn(500);
+                    $('.navbar').addClass('menu-bg');
+
+                } else {
+                    
+                    $('.navbar').removeClass('menu-bg');
+                }
+            });
+        
+    }
+	$('#bs-example-navbar-collapse-1').localScroll();
+
+
+		$.localScroll();
+
+
+
+});
